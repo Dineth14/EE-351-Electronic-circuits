43b05d5ae522e5b22bd561bdadfe9b6597631864
Dineth Perera <dp18perera@gmail.com>
2026-03-18 22:18:58 +0530

    Remove power simulation modules: dc_ac.py, dc_dc.py, loads.py, plotter.py, utils.py, and requirements.txt

--- power_sim/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Educational power electronics waveform simulation toolkit."""
-
-from . import ac_dc, dc_ac, dc_dc, loads, plotter, utils
-
-__all__ = ["ac_dc", "dc_ac", "dc_dc", "loads", "plotter", "utils"]
